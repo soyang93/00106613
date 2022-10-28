9860c662f6e4a223930d3d7525892c66af4eca07
Caio Barroso <caiobarroso511@yahoo.com.br>
2022-10-28 13:47:14 -0300

    logo image delay fixed

--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 function Input() {
   return (
     <div className="flex flex-row px-4 py-4  items-center">
-      <img src="/Frame22.png" alt="..." className="w-[63px] h-[68px]" />
+      <Image src="/Frame22.png" alt="..." width={63} height={68} />
       <div className="flex h-12 bg-[#3C3C3C] w-[392px] ml-4 rounded-md  px-2 items-center">
         <MagnifyingGlassIcon className="w-6 h-6 text-white" />
         <input
