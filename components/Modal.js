44851daa4beb3017db02ccc32cb214e01ef317a1
Caio Barroso <caiobarroso511@yahoo.com.br>
2022-11-01 19:23:49 -0300

    push 2

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,6 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Fragment } from "react";
 import {
   ClipboardDocumentCheckIcon,
